521d0823aaef08a5fe7e41b4a7773afafb78eae0
youness anflous <anayouness51@gmail.com>
2025-01-31 16:21:33 +0100

    update

--- src_checker/checker.c
+++ src_checker/checker.c
@@ -2,27 +2,27 @@
 
 int	execute_command(t_stack *stack_a, t_stack *stack_b, char *line)
 {
-	if (ft_strncmp(line, "sa", 2) == 0)
+	if (ft_strncmp(line, "sa", 2) == 0 && line[2] == '\n')
 		sa(stack_a, 0);
-	else if (ft_strncmp(line, "sb", 2) == 0)
+	else if (ft_strncmp(line, "sb", 2) == 0 && line[2] == '\n')
 		sb(stack_b, 0);
-	else if (ft_strncmp(line, "ss", 2) == 0)
+	else if (ft_strncmp(line, "ss", 2) == 0 && line[2] == '\n')
 		ss(stack_a, stack_b, 0);
-	else if (ft_strncmp(line, "pa", 2) == 0)
+	else if (ft_strncmp(line, "pa", 2) == 0 && line[2] == '\n')
 		pa(stack_a, stack_b, 0);
-	else if (ft_strncmp(line, "pb", 2) == 0)
+	else if (ft_strncmp(line, "pb", 2) == 0 && line[2] == '\n')
 		pb(stack_a, stack_b, 0);
-	else if (ft_strncmp(line, "ra", 2) == 0)
+	else if (ft_strncmp(line, "ra", 2) == 0 && line[2] == '\n')
 		ra(stack_a, 0);
-	else if (ft_strncmp(line, "rb", 2) == 0)
+	else if (ft_strncmp(line, "rb", 2) == 0 && line[2] == '\n')
 		rb(stack_b, 0);
-	else if (ft_strncmp(line, "rr", 2) == 0)
+	else if (ft_strncmp(line, "rr", 2) == 0 && line[2] == '\n')
 		rr(stack_a, stack_b, 0);
-	else if (ft_strncmp(line, "rra", 3) == 0)
+	else if (ft_strncmp(line, "rra", 3) == 0 && line[3] == '\n')
 		rra(stack_a, 0);
-	else if (ft_strncmp(line, "rrb", 3) == 0)
+	else if (ft_strncmp(line, "rrb", 3) == 0 && line[3] == '\n')
 		rrb(stack_b, 0);
-	else if (ft_strncmp(line, "rrr", 3) == 0)
+	else if (ft_strncmp(line, "rrr", 3) == 0 && line[3] == '\n')
 		rrr(stack_a, stack_b, 0);
 	else
 		return (1);
@@ -46,7 +46,8 @@ int	main(int argc, char **argv)
 	while (line)
 	{
 		if (execute_command(stack_a, stack_b, line))
-			return (free_stacks(stack_a, stack_b), free(line), 0);
+			return (write(1, "Error\n", 6),
+				free_stacks(stack_a, stack_b), free(line), 1);
 		free(line);
 		line = get_next_line(0);
 	}
